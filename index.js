fbeca3cd6d2864296fa413c8eb29d48e5f98ea9e
Ever <ever.m96@hotmail.com>
2023-04-19 07:22:24 -0400

    Functions continued lab.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+ 
+const mondayWork = function ( activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (string = "*") {
+    return function(value2 = "special") {
+        return `You are ${string}${value2}${string}!`
+    }
+}
+wrapAdjective("||")("a dedicated programmer")
